ec32508a9322e14971ade16e008cf6c814a12382
Padyaa <Padya@MacBook-Andrej.local>
2021-12-04 19:26:05 +0300

    rework

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,13 @@
 public class Main {
     public static void main(String[] args) {
         int balance = 4000;
-        int amount = 9000;
-        int bonus = 0;
+        int amount = 900;
+        int bonus;
 
         if (amount > 1000) {
             bonus = amount / 100;
+        } else {
+            bonus = 0;
         }
 
         int final_balane = balance + bonus + amount;
