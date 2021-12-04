ab4f35032c7ac23349863250148f0fb7a5e78896
Padyaa <Padya@MacBook-Andrej.local>
2021-12-04 19:07:17 +0300

    rework

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,14 @@
 public class Main {
     public static void main(String[] args) {
         int balance = 4000;
-        int amount =9000;
+        int amount = 9000;
         int bonus = 0;
 
-
         if (amount > 1000) {
-            bonus = amount/100;
+            bonus = amount / 100;
         }
+
         int final_balane = balance + bonus + amount;
-        System.out.println( "На вашем счету:"+final_balane );
+        System.out.println("На вашем счету:" + final_balane);
     }
 }
